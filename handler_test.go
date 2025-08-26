d03f6ab75b86f236af9d56cff72e4b5ae009b89f
widiand <widiand36@gmail.com>
2025-08-26 22:28:19 +0700

    request

--- handler_test.go
+++ handler_test.go
@@ -46,3 +46,19 @@ func TestServeMux(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.Method)
+		fmt.Fprint(w, r.RequestURI)
+	}
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: handler,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
