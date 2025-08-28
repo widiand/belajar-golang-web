140c73e62899d1226db3a83a81c85d55e35b2999
widiand <widiand36@gmail.com>
2025-08-28 22:24:34 +0700

    response code

---
+++ response_code_test.go
@@ -0,0 +1,34 @@
+package belajargolangweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func ResponseCode(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+
+	if name == "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(writer, "Name is empty")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+}
+
+func TestResponseCode(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080?name=eko", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
